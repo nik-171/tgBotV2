180b4070722279da9021d941671b13ec7d75e7c8
s1nglerrr <suvorkovtimur@gmail.com>
2025-07-05 12:37:38 +0300

    Update States.cs
    ЭЩКЕРЕ

--- HubBotTg/Core/Constants/States.cs
+++ HubBotTg/Core/Constants/States.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HubBotTg.Core.Constants
+public enum State
 {
-    internal class States
-    {
-    }
+    StateStart,
+    StatePostCreating,
+    StatePostConfirming,
+    StateGroupChosingForPostSanding
 }
